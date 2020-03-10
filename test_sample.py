572af1fef4e7940d741398eb211ebf0eff58f611
Bjarki Snær Magnússon <bjarkim@ru.is>
2020-03-10 11:28:32 +0000

    Added fizz_buzz function to sample and made a TODO in the test

---
+++ test_sample.py
@@ -0,0 +1,5 @@
+# TODO: FIZZBUZZ:
+#      1. Should return "Fizz" if number is divisable by 3
+#      2. Should return "Buzz" if number is divisable by 5
+#      3. Should return "FizzBuzz" if number is divisable by 3 and 5
+#      4. Should return the number if none of the other requirements are met
