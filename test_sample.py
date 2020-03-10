fcb8dd17eeb7f069676af16ab380e832cda28df1
Bjarki Snær Magnússon <bjarkim@ru.is>
2020-03-10 11:31:06 +0000

    Testing returns number failed

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,10 @@
+from sample import fizz_buzz
+
 # TODO: FIZZBUZZ:
 #      1. Should return "Fizz" if number is divisable by 3
 #      2. Should return "Buzz" if number is divisable by 5
 #      3. Should return "FizzBuzz" if number is divisable by 3 and 5
 #      4. Should return the number if none of the other requirements are met
+
+def test_returns_number_if_no_requirements_met():
+    assert fizz_buzz(2) == 2
