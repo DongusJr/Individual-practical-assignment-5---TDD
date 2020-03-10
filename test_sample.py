d92b91cb1c2d2784e6086c0864585889623c3bfb
Bjarki Snær Magnússon <bjarkim@ru.is>
2020-03-10 11:35:42 +0000

    Test for returns Buzz when divisable by 5, failed

--- test_sample.py
+++ test_sample.py
@@ -11,3 +11,6 @@ def test_returns_number_if_no_requirements_met():
 
 def test_returns_Fizz_if_number_divisable_by_three():
     assert fizz_buzz(9) == "Fizz"
+
+def test_returns_Buzz_if_number_divisable_by_five():
+    assert fizz_buzz(10) == "Buzz"
