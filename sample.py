6b28697219f466e1aeb0faf10ec23840c4bcbc5e
Bjarki Snær Magnússon <bjarkim@ru.is>
2020-03-10 11:36:33 +0000

    Test for returns Buzz when divisable by 5, passed

--- sample.py
+++ sample.py
@@ -2,4 +2,6 @@ def fizz_buzz(number):
     fizz_buzz_str = ""
     if number % 3 == 0:
         fizz_buzz_str += "Fizz"
+    if number % 5 == 0:
+        fizz_buzz_str += "Buzz"
     return fizz_buzz_str if fizz_buzz_str != "" else number
